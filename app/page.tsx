3fb2c0832c1360b4115170f26330435dacd07d16
LIONEL TRAORE <traore.lionel@gmail.com>
2025-08-20 16:28:27 +0000

    installation des modules nécessaires

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+     <main className="min-h-dvh grid place-items-center">
+      <h1 className="text-4xl font-bold">Hello Tailwind v4 👋</h1>
+    </main>
+  );
+}
